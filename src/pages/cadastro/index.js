fb3f4a88b882a728616046795d9018c24506dda4
Andre Perregil <andre_perregil@outlook.com>
2022-07-06 16:09:28 +0100

    refactor: Refatorar a logica de selecao de imagem de fundo, usando logica recursiva

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -3,23 +3,17 @@ import { useEffect, useState } from "react";
 /* import { Link } from "react-router-dom"; */
 
 function Cadastro() {
-  const [imagem, setImagem] = useState(
-    "https://images.unsplash.com/photo-1506126613408-eca07ce68773?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=799&q=80"
-  );
+  const todasImg = [
+    "https://images.unsplash.com/photo-1506126613408-eca07ce68773?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=799&q=80",
+    "https://images.unsplash.com/photo-1495791185843-c73f2269f669?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    "https://images.unsplash.com/photo-1583166614297-a97b68d5cead?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1315&q=80",
+  ];
+
+  const [imagem, setImagem] = useState(todasImg[0]);
 
   const escolheImagem = () => {
-    const todasImg = [
-      "https://images.unsplash.com/photo-1506126613408-eca07ce68773?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=799&q=80",
-      "https://images.unsplash.com/photo-1495791185843-c73f2269f669?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      "https://images.unsplash.com/photo-1583166614297-a97b68d5cead?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1315&q=80",
-    ];
     const index = Math.floor(Math.random() * todasImg.length);
-
-    return todasImg[index] !== imagem
-      ? todasImg[index]
-      : index === 0
-      ? todasImg[index + 1]
-      : todasImg[index - 1];
+    return todasImg[index] !== imagem ? todasImg[index] : escolheImagem();
   };
 
   useEffect(() => {
