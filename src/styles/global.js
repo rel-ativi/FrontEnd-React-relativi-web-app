1de77bb179d6a19ce5b11fde254f20abb04a33d5
Hugo Ernesto <mail.hbler@gmail.com>
2022-07-07 19:38:13 -0300

    feat: atualizando branch com merge da develop

--- src/styles/global.js
+++ src/styles/global.js
@@ -49,20 +49,9 @@ button{
     align-items: flex-start;
     flex-direction: column;
     gap:0.5rem;
+
+    border: 1px solid red;
 }
-`;
-
-export const Logo = styled.h1`
-  width: 100%;
-  padding: 0.5rem 0;
-
-  color: ${({ theme: { cores } }) => cores.secundaria};
-  font-family: "Source Code Pro", monospace;
-
-  span {
-    color: ${({ theme: { cores } }) => cores.marca};
-    font-family: "Source Code Pro", monospace;
-  }
 `;
 
 export const ContainerFlex = styled.div`
