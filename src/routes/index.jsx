9e246042bbb7b9cc3c69d5e38249c0fdee218c4e
Andre Perregil <andre_perregil@outlook.com>
2022-07-08 14:32:49 +0100

    fix: Resolvendo problemas com merge/develop

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Cadastro from "../pages/cadastro";
-=======
-import { Routes, Route } from "react-router-dom";
-import { Cadastro } from "../pages/cadastro";
->>>>>>> develop
 import { Dashboard } from "../pages/dashboard";
 import { Landing } from "../pages/landing";
 import { Login } from "../pages/login";
