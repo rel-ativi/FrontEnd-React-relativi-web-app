046a052bfb903a34122cbd5915bdd8efbec489da
Hugo Ernesto <mail.hbler@gmail.com>
2022-07-09 15:24:56 -0300

    feat: criados os toasts de sucesso e erro

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { ToastContainer } from "react-toastify";
 import Routes from "./routes";
 
 function App() {
   return (
     <>
       <Routes />
+      <ToastContainer />
     </>
   );
 }
