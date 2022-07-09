ed5d666291d7c2591aedb676bee1ecdf0c5f29a5
Hugo Ernesto <mail.hbler@gmail.com>
2022-07-09 01:58:12 -0300

    style: finalizada a estilização

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -1,52 +1,63 @@
 import { useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
 
 import Botao from "../../components/botao";
-import { LogoHorizontal } from "../../components/logo";
-import { ContainerFlex } from "../../styles/global";
-import { EstiloLanding, Background } from "./style";
+import Background from "../../components/background";
 
-import changeImageThunk from "../../store/modules/ImagesReel/thunks";
+import { LogoHorizontal, LogoQuadrado } from "../../components/logo";
+import { EstiloLanding } from "./style";
 
 export function Landing() {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const imagem = useSelector((store) => store.imagem);
-
-  useEffect(() => {
-    setTimeout(() => dispatch(changeImageThunk()), 5000);
-  }, [dispatch]);
 
   const redirecionaLogin = () => {
     navigate("/login", { replace: true });
   };
 
   return (
-    <Background url={imagem}>
+    <Background>
       <EstiloLanding>
-        <ContainerFlex flexDirection="column" alignItems="center" gap="20px">
-          <h1>Seja bem-vindo(a)!</h1>
-          <p>Saúde e bem estar com praticidade e planejamento!</p>
-        </ContainerFlex>
-        <ContainerFlex flexDirection="column" alignItems="flex-end">
-          <span>reaja</span>
-          <span>relaxe</span>
-          <span>rebole</span>
-          <span>respire</span>
-          <span>realize</span>
-        </ContainerFlex>
-        <ContainerFlex
-          flexDirection="row"
-          alignItems="flex-end"
-          justifyContent="space-around"
-        >
-          <LogoHorizontal largura="40%" />
-          <Botao tamanho="gg" onClick={redirecionaLogin}>
-            iniciar
-          </Botao>
-        </ContainerFlex>
+        <div className="container">
+          <div>
+            <p>
+              re<span>aja</span>
+            </p>
+            <p>
+              re<span>laxe</span>
+            </p>
+            <p>
+              re<span>bole</span>
+            </p>
+            <p>
+              res<span>pire</span>
+            </p>
+            <p>
+              re<span>alize</span>
+            </p>
+          </div>
+          <section>
+            <div className="logoMobile">
+              <LogoQuadrado largura="7rem" />
+            </div>
+            <div className="logoTablet">
+              <LogoHorizontal largura="20rem" />
+            </div>
+            <div className="logoDesktop">
+              <LogoHorizontal largura="30rem" />
+            </div>
+            <div>
+              <p>
+                Bem-vindo(a)<span>!</span>
+              </p>
+              <p>
+                Aqui você encontra saúde e bem estar com praticidade e
+                planejamento!
+              </p>
+              <Botao tamanho="gg" larguraFixa="90%" onClick={redirecionaLogin}>
+                iniciar
+              </Botao>
+            </div>
+          </section>
+        </div>
       </EstiloLanding>
     </Background>
   );
