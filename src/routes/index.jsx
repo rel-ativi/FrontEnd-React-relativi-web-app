0a9ba29eae371de823548ae87f03a68c18e44e54
Andre Perregil <andre_perregil@outlook.com>
2022-07-10 15:48:16 +0100

    feat: agora token e' verificado na criacao de rotas, quando existente routes cadastro, landing login tem o conteudo da loja

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,19 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useNavigate, BrowserRouter, Routes, Route } from "react-router-dom";
 import { Cadastro } from "../pages/cadastro";
 import { Dashboard } from "../pages/dashboard";
 import { Landing } from "../pages/landing";
 import { Login } from "../pages/login";
 import { Loja } from "../pages/Loja";
+import {} from "react-router-dom";
 
 function Router() {
+  const verificaToken = (component) => {
+    const token = localStorage.getItem("@relativi:token");
+    return token ? <Loja /> : component;
+  };
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={"/"} index element={<Landing />} />
-        <Route path={"/cadastro"} element={<Cadastro />} />
-        <Route path={"/login"} element={<Login />} />
         <Route path={"/loja"} element={<Loja />} />
         <Route path={"/dashboard"} element={<Dashboard />} />
+        <Route path={"/"} index element={verificaToken(<Landing />)} />
+        <Route path={"/cadastro"} element={verificaToken(<Cadastro />)} />
+        <Route path={"/login"} element={verificaToken(<Login />)} />
       </Routes>
     </BrowserRouter>
   );
