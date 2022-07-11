507f6690008ff475e158052d02b6346138085b28
Kamila Mattos <99353050+KamilaMattos@users.noreply.github.com>
2022-07-11 15:03:45 -0300

    style: aplicado mobile first

--- src/components/footer/style.js
+++ src/components/footer/style.js
@@ -1,30 +1,48 @@
 import styled from "styled-components"
+import { mq } from "../../styles/global"
 
 export const EstiloFooter = styled.footer`
   background-color: ${({ theme: { cores } }) => cores.secundaria};
   color: ${({ theme: { cores } }) => cores.primaria};
   width: 100%;
 
-  .container {
-    display: flex;
-    align-items: center;
-    flex-direction: row;
-    justify-content: space-between;
-
-    ul {
+  ${mq[1]} {
+    .container {
       display: flex;
-      gap: 30px;
+      flex-direction: column;
+      flex-wrap: wrap;
+      justify-content: space-between;
+
+      ul {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        flex-wrap: wrap;
+        width: 95%;
+
+        span {
+          color: ${({ theme: { cores } }) => cores.suporteDois};
+          font-size: 0.7rem;
+          cursor: auto;
+        }
+      }
 
       span {
-        color: ${({ theme: { cores } }) => cores.suporteDois};
-        font-size: 0.7rem;
-        cursor: auto;
+        color: ${({ theme: { cores } }) => cores.marca};
+        cursor: pointer;
       }
     }
+  }
 
-    span {
-      color: ${({ theme: { cores } }) => cores.marca};
-      cursor: pointer;
+  ${mq[2]} {
+    ul {
+      width: 70%;
+    }
+  }
+
+  ${mq[3]} {
+    ul {
+      width: 100%;
     }
   }
 `
